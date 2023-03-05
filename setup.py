5e5d6e60bce242f1a3df6b6ddb5b14bdac863773
denisneuf <denisneuf@hotmail.com>
2023-03-05 10:32:33 +0800

    v0.0.6

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ if REQUIRED_PYTHON_VERSION > CURRENT_PYTHON_VERSION:
 
 setup(
     name='python-paypal-api',
-    version='0.0.5',
+    version='0.0.6',
     python_requires='>=3.8',
     author='denisneuf',
     author_email='denisneuf@hotmail.com',
